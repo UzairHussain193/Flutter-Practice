235d23b6de7c894e49a7de137e8a4d16663a1d8d
Uzair Hussain <uzairhussain193@gmail.com>
2024-08-15 09:09:56 +0500

    feat: Add SignUp widget to Forms.dart

---
+++ lib/Forms.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+  @override
+  State<StatefulWidget> createState() => SignUpState();
+}
+class SignUpState extends State<SignUp>{
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
